9bf7d2f0145427c5b38c257b3bdefd8f7cf5c53a
Shahriyor <shahriyorabdullayev2000@gmail.com>
2022-07-16 10:52:27 +0500

    initial commit

---
+++ app/src/main/java/com/example/androidsaas/fragment/main/likes/LikesFragment.kt
@@ -0,0 +1,14 @@
+package com.example.androidsaas.fragment.main.likes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidsaas.R
+
+
+class LikesFragment : Fragment(R.layout.fragment_likes) {
+
+
+}
